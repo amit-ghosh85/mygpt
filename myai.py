118c4950d5707f60224dfd0cefa808989f77ec1a
amitkumarghosh <professionalakg@gmail.com>
2024-01-30 16:53:54 +0530

    inpmsg

--- myai.py
+++ myai.py
@@ -5,7 +5,7 @@ os.environ['GOOGLE_API_KEY']="AIzaSyAqtRmOxpAqDC1NRV0T-2M2TxQqazgImQ0"
 ai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 model=ai.GenerativeModel('gemini-pro')
 st.title("Welcome to Amit's GPT")
-question=st.text_area('Please enter your questions')
+question=st.text_area('Please enter your questions',"type your question!!!!")
 
 # print(response.prompt_feedback)
 # print(response.text)
